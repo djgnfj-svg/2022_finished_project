39210e199d436b5d7397cec7c923af828432b8c8
djgnfj-svg <djgnfj8923@naver.com>
2022-04-05 01:12:28 +0900

    add DRF remove, update

--- backend/Re_API/api/apis.py
+++ backend/Re_API/api/apis.py
@@ -1,9 +1,10 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 from .serializers import UserDataCreateSerializer, UserDataSerializer
 from ..models import ER_Base_Model
-
+from rest_framework import exceptions
 class UserDataViewSet(viewsets.ModelViewSet):
 	queryset = ER_Base_Model.objects.filter().order_by("id")
 	serializer_class = UserDataSerializer
@@ -26,8 +27,22 @@ class UserDataViewSet(viewsets.ModelViewSet):
 		serializer = UserDataSerializer(queryset, many=True)
 		return Response(serializer.data)
 
-	def update(self, request, *args, **kwargs):
-		return super().update(request, *args, **kwargs)
+	def update(self, request, pk=None):
+		print(request.data.get("remove_password"))
+		serializer = UserDataCreateSerializer(data=request.data)
+		if serializer.is_valid():
+			rtn = serializer.change(request, serializer.data, pk)
+			return Response(UserDataSerializer(rtn).data, status=status.HTTP_201_CREATED)
+		#is_valid 하지 않으면
+		return Response({"msg" : "test"},status=status.HTTP_400_BAD_REQUEST)
 
-	def destroy(self, request, *args, **kwargs):
-		return super().destroy(request, *args, **kwargs)
+	def destroy(self, request,  pk=None,):
+		remove_password = request.query_params.get("remove_password",None)
+		if not remove_password:
+			raise Http404
+			# raise exceptions.ValidationError("비밀번호가 유효하지 않습니다.", status=status.HTTP_400_BAD_REQUEST)
+		queryset = self.get_queryset().filter(id=pk, remove_password = remove_password)
+		if not queryset.exists():
+			raise Http404
+		queryset.delete()
+		return Response({"msg": "ok"})
--- backend/Re_API/api/serializers.py
+++ backend/Re_API/api/serializers.py
@@ -1,14 +1,13 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 from ..ER_API_utils import get_ER_api_data
 
 from ..models import ER_Base_Model
-
 class UserDataSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ER_Base_Model
-		# fields = ('__all__')
-		exclude = ("id",)
+		fields = ('__all__')
+		# exclude = ("id",)
 
 #todo postman으로 실험하기
 class UserDataCreateSerializer(serializers.Serializer):
@@ -31,3 +30,17 @@ class UserDataCreateSerializer(serializers.Serializer):
 			else:
 				pass
 		return instance
+	
+	def change(self, request, data, id, commit=True):
+		instance = ER_Base_Model.objects.filter(id=id).first()
+		if instance.remove_password != data.get("remove_password",None):
+			raise exceptions.ValidationError({"error_msg" : "비밀번호가 일치하지 않습니다."})
+		instance.memo = data.get("memo", None)
+		if commit:
+			try:
+				instance.save()
+			except Exception as e:
+				print(e)
+			else:
+				pass
+		return instance		
